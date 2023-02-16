3ff2fb7afdab8fa7eb1f07c2ced72356ddf7dd1f
Anil Maharjan <1720245+dejavu1987@users.noreply.github.com>
2023-02-16 15:04:19 +0100

    COP

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    global: ["d3"],
+  },
+  extends: "airbnb-base",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
